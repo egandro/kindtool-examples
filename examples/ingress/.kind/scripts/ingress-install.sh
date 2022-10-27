1b2f4ef5333bc228259ae1226bfa9220a6d16b78
Harald Fielker <harald.fielker@gmail.com>
2022-10-27 07:54:12 +0200

    .

--- examples/ingress/.kind/scripts/ingress-install.sh
+++ examples/ingress/.kind/scripts/ingress-install.sh
@@ -7,11 +7,20 @@ kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
 
 # https://reuvenharrison.medium.com/how-to-wait-for-a-kubernetes-pod-to-be-ready-one-liner-144bbbb5a76f
 
-echo "waiting for contour to become ready"
+echo "waiting for 'contour' to become ready"
 while true; do
     kubectl get pods \
             --namespace projectcontour \
             -l app=contour \
+            -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' 2>/dev/null | grep -o True >/dev/null && break
+    sleep 1
+done
+
+echo "waiting for 'envoy' to become ready"
+while true; do
+    kubectl get pods \
+            --namespace projectcontour \
+            -l app=envoy \
             -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' 2>/dev/null | grep -o True >/dev/null && break
     sleep 1
 done
